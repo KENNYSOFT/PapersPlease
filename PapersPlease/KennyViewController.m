94dea059f7a7fcffcc5f2db9a63933b671da9c4c
박현민 <525hm@naver.com>
2014-11-24 10:42:51 +0900

    Add Assets, Button Click Image, Change Screen

--- PapersPlease/KennyViewController.m
+++ PapersPlease/KennyViewController.m
@@ -18,12 +18,39 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    [endless setHidden:YES];
+    
+    if(audioPlayer==nil)
+    {
+        audioPlayer=[[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"Theme" ofType:@"mp3"]] error:nil];
+        [audioPlayer setNumberOfLoops:-1];
+        [audioPlayer prepareToPlay];
+    }
+    [audioPlayer play];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)mainmenuEndlessButtonClicked:(id)sender
+{
+    [mainmenu setHidden:YES];
+    [endless setHidden:NO];
+}
+
+-(IBAction)endlessBackButtonClicked:(id)sender
+{
+    [endless setHidden:YES];
+    [mainmenu setHidden:NO];
+}
+
+-(IBAction)endlessStartButtonClicked:(id)sender
+{
+    //
 }
 
 @end
